43dfcc46b398dba9399ceba90600b0b42a666bc1
maddybfuturesfund <43098090+maddybfuturesfund@users.noreply.github.com>
2023-10-18 18:26:11 -0500

    edited login page; added photo

--- src/routes/createLogIn.js
+++ src/routes/createLogIn.js
@@ -1,16 +1,19 @@
 import { useState } from "react";
-
+import Navbar from "./NavBar.js"; 
 
 function App() {
   return (
     <>
     <div>
-      <button onClick={() => {console.log('click')}}>
-      Log In</button>
+        <img src = "Fisklogo.png"></img>
+    </div>
+    <div>
      <input onChange={(e) => {
       console.log(e.target.value)
      }} type = "text"></input>
      <input type = "password"></input>
+     <button onClick={() => {console.log('click')}}>
+      Log In</button>
     </div>
     </>
     

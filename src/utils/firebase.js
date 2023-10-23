59e043293a3cdd2d772e6532ef9773c7d5befde2
maddybfuturesfund <43098090+maddybfuturesfund@users.noreply.github.com>
2023-10-23 16:58:07 -0500

    created firebase file

---
+++ src/utils/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "./firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCWReDRCN3szdhefs9PshevaAYEMgximjA",
+  authDomain: "fisk-events-board.firebaseapp.com",
+  projectId: "fisk-events-board",
+  storageBucket: "fisk-events-board.appspot.com",
+  messagingSenderId: "316693740020",
+  appId: "1:316693740020:web:9deb54efed4aa06ea9e1a1",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;

588984b57417595e022584300ee6870a094ebdb1
nanakwame-1 <146495863+nanakwame-1@users.noreply.github.com>
2023-11-01 23:40:46 +0000

    created event page

---
+++ src/routes/createEvent.js
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+
+function CreateEventPage() {
+  const [eventDetails, setEventDetails] = useState({
+    eventName: '',
+    eventDate: '',
+    eventDescription: '',
+  });
+
+  const [flyer, setFlyer] = useState(null);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setEventDetails({
+      ...eventDetails,
+      [name]: value,
+    });
+  };
+
+  const handleFlyerChange = (e) => {
+    const file = e.target.files[0];
+    setFlyer(file);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // You can handle form submission here, e.g., sending the data and flyer to a server.
+    console.log('Submitted Event Details:', eventDetails);
+    console.log('Uploaded Flyer:', flyer);
+  };
+
+  return (
+    <div>
+      <h2>Create Event</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="eventName">Event Name:</label>
+          <input
+            type="text"
+            id="eventName"
+            name="eventName"
+            value={eventDetails.eventName}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="eventDate">Event Date:</label>
+          <input
+            type="date"
+            id="eventDate"
+            name="eventDate"
+            value={eventDetails.eventDate}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="eventDescription">Event Description:</label>
+          <textarea
+            id="eventDescription"                                 
+            name="eventDescription"
+            value={eventDetails.eventDescription}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="flyer">Upload Flyer:</label>
+          <input
+            type="file"
+            id="flyer"
+            name="flyer"
+            accept=".jpg, .jpeg, .png, .pdf"
+            onChange={handleFlyerChange}
+          />
+        </div>
+        <div>
+          <button type="submit">Create Event</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default CreateEventPage;
+

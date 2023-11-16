3bc146d530e080055f3fe13aa2588d2c19a13157
maddybfuturesfund <43098090+maddybfuturesfund@users.noreply.github.com>
2023-11-15 18:00:00 -0600

    created loading state

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,12 @@
+import { useState } from "react";
+
 export default function Index() {
+  const [loading, setLoading] = useState(true);
 
   return (
-  <h1>Fisk events board</h1>
-  
-  )
+    <>
+      <h1>Fisk events board</h1>
+      {loading ? "loading" : <h2>Events</h2>}
+    </>
+  );
 }

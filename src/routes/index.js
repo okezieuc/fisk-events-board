27af591db2298c6078a50c502c72b3951e2945bb
maddybfuturesfund <43098090+maddybfuturesfund@users.noreply.github.com>
2023-11-15 18:14:48 -0600

    events data connected

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+import app from "../utils/firebase";
+
+const db = getFirestore(app);
 
 export default function Index() {
   const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    async function getEventsData() {
+      const querySnapshot = await getDocs(collection(db, "events"));
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, " => ", doc.data());
+      });
+    }
+    getEventsData();
+  }, []);
 
   return (
     <>

277c1400c5765422c8446d14ccae38e3b717fa55
maddybfuturesfund <43098090+maddybfuturesfund@users.noreply.github.com>
2023-11-15 18:34:48 -0600

    display events

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,21 +6,38 @@ const db = getFirestore(app);
 
 export default function Index() {
   const [loading, setLoading] = useState(true);
+  const [events, setEvents] = useState([]);
+
   useEffect(() => {
     async function getEventsData() {
       const querySnapshot = await getDocs(collection(db, "events"));
+      const accumulator = [];
       querySnapshot.forEach((doc) => {
         // doc.data() is never undefined for query doc snapshots
-        console.log(doc.id, " => ", doc.data());
+        accumulator.push(doc.data());
       });
+      setEvents(accumulator);
+      setLoading(false);
     }
+
     getEventsData();
   }, []);
 
   return (
     <>
       <h1>Fisk events board</h1>
-      {loading ? "loading" : <h2>Events</h2>}
+      {loading ? (
+        "loading"
+      ) : (
+        <>
+          {events.map((event) => (
+            <div>
+              <div>{event.eventName}</div>
+              <div>{event.eventDescription}</div>
+            </div>
+          ))}
+        </>
+      )}
     </>
   );
 }

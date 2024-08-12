bc062d0ff7f29c59d34e25f497e6d1186c950858
okezieuc <53785400+okezieuc@users.noreply.github.com>
2024-08-12 15:44:23 -0500

    Create a container for the user-facing application
    This container restricts the width of the application on large devices.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import FirestoreImage from "../components/FirestoreImage";
-
+import AppContainer from "../components/AppContainer";
 import { fetchEventsData } from "../services/firebase";
 import type { Event } from "../services/firebase";
 
@@ -10,10 +10,10 @@ export default function Home() {
   const [error, setError] = useState<Error | null>(null);
 
   useEffect(() => {
-    const getEventsData  = async () => {
+    const getEventsData = async () => {
       // ensures is loading
       setLoading(true);
-      
+
       try {
         // gets the events and stores them
         const _events = await fetchEventsData();
@@ -29,29 +29,64 @@ export default function Home() {
         // ensures is not loading
         setLoading(false);
       }
-    }
+    };
 
     getEventsData();
   }, []);
 
   return (
-    <div style={{ maxWidth: '800px', margin: '0 auto', display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)', gap: '15px' }}>
-      <h1 style={{ marginBottom: '20px', textAlign: 'center', gridColumn: '1 / -1' }}>Fisk events board</h1>
-      {loading ? (
-        "Loading..."
-      ) : error ? (
-        <>{error}</>
-      ) :(
-        <>
-          {events.map((event) => (
-            <div key={event.id} style={{ padding: '10px', border: '1px solid #ccc', borderRadius: '10px' }}>
-              {event.flyerStorageURL && <FirestoreImage src={event.flyerStorageURL.toString()} />}
-              <div style={{ marginBottom: '5px', fontSize: '1.2em', fontWeight: 'bold' }}>{event.name}</div>
-              <div>{event.description}</div>
-            </div>
-          ))}
-        </>
-      )}
-    </div>
+    <AppContainer>
+      <div
+        style={{
+          maxWidth: "800px",
+          margin: "0 auto",
+          display: "grid",
+          gridTemplateColumns: "repeat(2, 1fr)",
+          gap: "15px",
+        }}
+      >
+        <h1
+          style={{
+            marginBottom: "20px",
+            textAlign: "center",
+            gridColumn: "1 / -1",
+          }}
+        >
+          Fisk events board
+        </h1>
+        {loading ? (
+          "Loading..."
+        ) : error ? (
+          <>{error}</>
+        ) : (
+          <>
+            {events.map((event) => (
+              <div
+                key={event.id}
+                style={{
+                  padding: "10px",
+                  border: "1px solid #ccc",
+                  borderRadius: "10px",
+                }}
+              >
+                {event.flyerStorageURL && (
+                  <FirestoreImage src={event.flyerStorageURL.toString()} />
+                )}
+                <div
+                  style={{
+                    marginBottom: "5px",
+                    fontSize: "1.2em",
+                    fontWeight: "bold",
+                  }}
+                >
+                  {event.name}
+                </div>
+                <div>{event.description}</div>
+              </div>
+            ))}
+          </>
+        )}
+      </div>
+    </AppContainer>
   );
 }

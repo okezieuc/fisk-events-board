0303e0fd34e7747136f2a23396d72b7b08ea0298
Benjamin Bamisile <bambobtim@gmail.com>
2024-08-16 21:13:05 -0500

    navigation arrows clean

--- src/components/TodayViewEventNavigationBar.tsx
+++ src/components/TodayViewEventNavigationBar.tsx
@@ -6,38 +6,6 @@ type TodayViewEventNavigationBarProps = {
   currentSlide: number;
 };
 
-// TODO: Replace the text here with actual icons.
-// TODO: Create a custom shadow option for the buttons or explore alternatives.
-// TODO: Replace the text here with actual icons.
-// TODO: Create a custom shadow option for the buttons or explore alternatives.
-// export default function TodayViewEventNavigationBar(
-//   props: TodayViewEventNavigationBarProps
-// ) {
-//   return (
-//     <div className="flex">
-//       <button
-//         className="text-white bg-black p-2 rounded text-xs"
-//         onClick={() =>
-//           props.setCurrentEventIndex(
-//             (index) => (index + props.eventCount - 1) % props.eventCount
-//           )
-//         }
-//       >
-//         Left
-//       </button>
-//       <div className="grow"></div>
-//       <button
-//         className="text-white bg-black p-2 rounded text-xs"
-//         onClick={() =>
-//           props.setCurrentEventIndex((index) => (index + 1) % props.eventCount)
-//         }
-//       >
-//         Right
-//       </button>
-//     </div>
-//   );
-// }
-
 export default function TodayViewEventNavigationBar(
   props: TodayViewEventNavigationBarProps
 ) {
